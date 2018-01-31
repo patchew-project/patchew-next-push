e314548d9645d23586d9c3484d24f9e10741915a
Klim Kireev <klim.kireev@virtuozzo.com>
2018-01-31 19:25:21 +0300

    vnc: fix segfault in closed connection handling
    On one of our client's node, due to trying to read from closed ioc,
    a segmentation fault occured. Corresponding backtrace:
    
    0  object_get_class (obj=obj@entry=0x0)
    1  qio_channel_readv_full (ioc=0x0, iov=0x7ffe55277180 ...
    2  qio_channel_read (ioc=<optimized out> ...
    3  vnc_client_read_buf (vs=vs@entry=0x55625f3c6000, ...
    4  vnc_client_read_plain (vs=0x55625f3c6000)
    5  vnc_client_read (vs=0x55625f3c6000)
    6  vnc_client_io (ioc=<optimized out>, condition=G_IO_IN, ...
    7  g_main_dispatch (context=0x556251568a50)
    8  g_main_context_dispatch (context=context@entry=0x556251568a50)
    9  glib_pollfds_poll ()
    10 os_host_main_loop_wait (timeout=<optimized out>)
    11 main_loop_wait (nonblocking=nonblocking@entry=0)
    12 main_loop () at vl.c:1909
    13 main (argc=<optimized out>, argv=<optimized out>, ...
    
    Having analyzed the coredump, I understood that the reason is that
    ioc_tag is reset on vnc_disconnect_start and ioc is cleaned
    in vnc_disconnect_finish. Between these two events due to some
    reasons the ioc_tag was set again and after vnc_disconnect_finish
    the handler is running with freed ioc,
    which led to the segmentation fault.
    
    The patch checks vs->disconnecting in places where we call
    qio_channel_add_watch to prevent such an occurrence.
    
    Signed-off-by: Klim Kireev <klim.kireev@virtuozzo.com>
    Message-Id: <20180131162521.31210-1-klim.kireev@virtuozzo.com>

--- ui/vnc.c
+++ ui/vnc.c
@@ -1398,13 +1398,19 @@ static void vnc_client_write_locked(VncState *vs)
     } else
 #endif /* CONFIG_VNC_SASL */
     {
-        vnc_client_write_plain(vs);
+        if (vs->disconnecting == FALSE) {
+            vnc_client_write_plain(vs);
+        } else {
+            if (vs->ioc_tag != 0) {
+                g_source_remove(vs->ioc_tag);
+                vs->ioc_tag = 0;
+            }
+        }
     }
 }
 
 static void vnc_client_write(VncState *vs)
 {
-
     vnc_lock_output(vs);
     if (vs->output.offset) {
         vnc_client_write_locked(vs);
@@ -1412,8 +1418,12 @@ static void vnc_client_write(VncState *vs)
         if (vs->ioc_tag) {
             g_source_remove(vs->ioc_tag);
         }
-        vs->ioc_tag = qio_channel_add_watch(
-            vs->ioc, G_IO_IN, vnc_client_io, vs, NULL);
+        if (vs->disconnecting == FALSE) {
+            vs->ioc_tag = qio_channel_add_watch(
+                vs->ioc, G_IO_IN, vnc_client_io, vs, NULL);
+        } else {
+            vs->ioc_tag = 0;
+        }
     }
     vnc_unlock_output(vs);
 }
