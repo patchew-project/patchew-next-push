053ca9a84f418405412084801bec98345da9ea0b
Miroslav Rezanina <mrezanin@redhat.com>
2018-03-05 16:42:55 +0100

    Use CONFIG_VHOST_USER_SCSI to enable vhost-scsi-pci
    Currently, "defined(CONFIG_VHOST_USER) && defined(CONFIG_LINUX)"
    is used to enable/disable vhost-scsi-pci build as this condition
    is used to enable/disable CONFIG_VHOST_USER_SCSI too.
    
    However, when CONFIG_VHOST_USER_SCSI is manually disable vhost-scsi-pci
    is still registered although it should be disabled.
    
    Changing condition to use CONFIG_VHOST_USER_SCSI so vhost-scsi-pci
    can be controled by CONFIG_VHOST_USER_SCSI option.
    
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>
    Message-Id: <1520264575-26506-1-git-send-email-mrezanin@redhat.com>
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- hw/virtio/virtio-pci.c
+++ hw/virtio/virtio-pci.c
@@ -2163,7 +2163,7 @@ static const TypeInfo vhost_scsi_pci_info = {
 };
 #endif
 
-#if defined(CONFIG_VHOST_USER) && defined(CONFIG_LINUX)
+#if defined(CONFIG_VHOST_USER_SCSI)
 /* vhost-user-scsi-pci */
 static Property vhost_user_scsi_pci_properties[] = {
     DEFINE_PROP_UINT32("vectors", VirtIOPCIProxy, nvectors,
@@ -2696,7 +2696,7 @@ static void virtio_pci_register_types(void)
 #ifdef CONFIG_VHOST_SCSI
     type_register_static(&vhost_scsi_pci_info);
 #endif
-#if defined(CONFIG_VHOST_USER) && defined(CONFIG_LINUX)
+#if defined(CONFIG_VHOST_USER_SCSI)
     type_register_static(&vhost_user_scsi_pci_info);
 #endif
 #ifdef CONFIG_VHOST_VSOCK
