ddce05a939bf10d3569b0333be39074a7b84f28a
Michael Walle <michael@walle.cc>
2018-05-21 14:21:00 +0200

    target/lm32: hold BQL in gdbstub
    Changing the IP/IM registers may cause interrupts, so hold the BQL.
    
    Cc: qemu-stable@nongnu.org
    Signed-off-by: Michael Walle <michael@walle.cc>
    Message-Id: <20180521122100.22602-3-michael@walle.cc>

--- target/lm32/gdbstub.c
+++ target/lm32/gdbstub.c
@@ -18,6 +18,7 @@
  * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 #include "qemu/osdep.h"
+#include "qemu/main-loop.h"
 #include "qemu-common.h"
 #include "cpu.h"
 #include "exec/gdbstub.h"
@@ -82,10 +83,14 @@ int lm32_cpu_gdb_write_register(CPUState *cs, uint8_t *mem_buf, int n)
             env->ie = tmp;
             break;
         case 37:
+            qemu_mutex_lock_iothread();
             lm32_pic_set_im(env->pic_state, tmp);
+            qemu_mutex_unlock_iothread();
             break;
         case 38:
+            qemu_mutex_lock_iothread();
             lm32_pic_set_ip(env->pic_state, tmp);
+            qemu_mutex_unlock_iothread();
             break;
         }
     }
